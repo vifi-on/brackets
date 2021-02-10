1a3bef22d9a3b1f3f9ca738eccefbbab8df07286
vifi-on <ladiasman1992@gmail.com>
2021-02-10 15:42:15 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsArr = bracketsConfig.map(el => el.join(''));
+
+  for(let i = 0; i < bracketsArr.length; i++){
+    if(str.includes(bracketsArr[i])){
+      str = str.split(bracketsArr[i]).join('');
+      i = -1;
+    }
+  }
+
+  return !str ? true : false
 }
